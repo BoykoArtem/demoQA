9569e7e82ef996a9d7ad8323fbeed11da13a9c5a
Artem Boyko <bro7times@gmail.com>
2023-08-23 18:45:41 +0300

    Почти финальная версия.

--- src/test/java/model/BasePage.java
+++ src/test/java/model/BasePage.java
@@ -2,13 +2,9 @@ package model;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public abstract class BasePage {
     private final WebDriver driver;
-    private WebDriverWait wait2;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -16,11 +12,5 @@ public abstract class BasePage {
     }
     protected WebDriver getDriver() {
         return driver;
-    }
-    protected WebDriverWait getWait2() {
-        if (wait2 == null) {
-            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2), Duration.ofMillis(200));
-        }
-        return wait2;
     }
 }
